214a266d7615a8f412ec192bcc034220bc67d1e9
Johnny Rojas Cifra <157323400+Johnnyrc26@users.noreply.github.com>
2024-09-10 18:33:57 +0200

    Fix: Update apiDataHandler.js, index.js, and apiData.js to handle MongoDB connection and errors

--- api/logic/apiData.js
+++ api/logic/apiData.js
@@ -1,31 +1,25 @@
 import 'dotenv/config'
 import fetch from 'node-fetch'
 import { Room } from '../data/index.js'
-import mongoose from 'mongoose'
 
-const { API_VQ, MONGODB_URL } = process.env
-
-const connectToDatabase = () => {
-  return mongoose.connect(MONGODB_URL)
-}
+const { API_VQ } = process.env
 
 const apiData = () => {
   return fetch(API_VQ)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Error de red al intentar conectar con la API');
+        throw new Error('Error de red al intentar conectar con la API')
       }
-      return response.json();
+      return response.json()
     })
     .then(data => {
-      const pois = data.data.pois;
+      const pois = data.data.pois
 
       const savePromises = pois.map(poi => {
         return Room.findOne({ nameRoom: poi.nombre })
           .then(existing => {
             if (existing) {
-              console.log('El hotel ya existe');
-              return null; // Skip saving
+              return null
             }
             const hotel = new Room({
               nameRoom: poi.nombre,
@@ -40,33 +34,20 @@ const apiData = () => {
               instagram: poi.instagram,
               manager: '66d878adbb0fb67868b4bfc1',
               imagenes: poi.imagenes.map(imagen => ({ url: imagen.url })),
-            });
+            })
 
-            return hotel.save();
-          });
-      });
+            return hotel.save()
+          })
+      })
 
-      return Promise.all(savePromises);
+      return Promise.all(savePromises)
     })
     .then(() => {
-      console.log('Datos guardados correctamente en la base de datos');
+      console.log('Datos guardados correctamente en la base de datos')
     })
     .catch(error => {
-      console.error('Error al procesar los datos:', error);
-    });
-};
-
-// Ejecutar fetchData() al iniciar el script y configurar el cron job
-connectToDatabase()
-  .then(() => {
-    console.log('Conexión exitosa a MongoDB');
-    return apiData();
-  })
-  .catch(error => {
-    console.error('Error al conectar con MongoDB:', error);
-  })
-  .finally(() => {
-    mongoose.connection.close();
-  });
+      console.error('Error al procesar los datos:', error)
+    })
+}
 
 export default apiData
