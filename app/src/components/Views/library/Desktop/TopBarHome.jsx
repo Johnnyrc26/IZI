c5217b35d7b508eb6baf379602724bf9098ed5fd
Johnny Rojas Cifra <157323400+Johnnyrc26@users.noreply.github.com>
2024-09-13 17:18:19 +0200

    Update package files, add new images, and modify various components and styles
    - Updated package-lock.json and package.json
    - Added new images to the public directory
    - Created new components: Banner.jsx, TopBarHome.jsx, and corresponding CSS files
    - Updated RoomList.jsx and associated CSS for improved layout
    - Modified TopBar.css and Home.jsx for layout and design adjustments
    - Updated Tailwind configuration and index.css #2

---
+++ app/src/components/Views/library/Desktop/TopBarHome.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import './TopBarHome.css'; // Asegúrate de crear este archivo para los estilos
+
+const images = [
+  'https://cdn.laguiavalentinaquintero.com/poi/Poi_5772758682c805563ef06b95_c24d3d8ec63a80d22111689096b41776.jpeg',
+  'https://cdn.laguiavalentinaquintero.com/poi/Poi_5772758682c805563ef06bac_67ba4d3908e4d28fc33333ef14da39e0.jpeg',
+  'https://cdn.laguiavalentinaquintero.com/poi/Poi_5772758682c805563ef06ee1_6f5d6aef416a08633de792b9d89ee589.jpeg',
+  'https://cdn.laguia.grupoapok.com/poi/Poi_5772758682c805563ef06e36_b8b1ea83cf1a628a7d6520e6ba11eab1.jpeg'
+
+];
+
+const TopBarHome = () => {
+  const [currentImage, setCurrentImage] = useState(images[0]);
+  const [city, setCity] = useState('');
+  const [dates, setDates] = useState({ start: '', end: '' });
+  const [people, setPeople] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => {
+        const currentIndex = images.indexOf(prevImage);
+        const nextIndex = (currentIndex + 1) % images.length;
+        return images[nextIndex];
+      })
+    }, 5000)
+
+    return () => clearInterval(interval)
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log('Searching for:', { city, dates, people });
+  };
+
+  return (
+    <div className="topbar" style={{ backgroundImage: `url(${currentImage})` }}>
+      <div className="brand">KAU</div>
+      <div className="nav">
+        <a href="/register">Registrarse |</a>
+        <a href="/login"> Iniciar sesión |</a>
+        <a href="/about"> Conócenos</a>
+      </div>
+      <div className='searchBar'>
+      <form className="search-form" onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Ciudad"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <input
+          type="date"
+          value={dates.start}
+          onChange={(e) => setDates((prevDates) => ({ ...prevDates, start: e.target.value }))}
+        />
+        <input
+          type="date"
+          value={dates.end}
+          onChange={(e) => setDates((prevDates) => ({ ...prevDates, end: e.target.value }))}
+        />
+        <input
+          type="number"
+          placeholder="Número de personas"
+          value={people}
+          onChange={(e) => setPeople(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+      </div>
+    </div>
+  );
+};
+
+export default TopBarHome
